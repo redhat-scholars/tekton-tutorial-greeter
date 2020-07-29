fa37fadd0ede70913e344613b53fd4d06f3480ed
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-07-29 22:29:28 +0530

    trigger example

--- src/main/java/com/redhat/developers/GreetingResource.java
+++ src/main/java/com/redhat/developers/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello World!";
+        return "😺 Tekton Rocks!";
     }
 }

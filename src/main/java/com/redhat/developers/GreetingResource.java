f1130007afa8d777798bfc368ab988c8f95031c2
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-07-29 22:45:48 +0530

    revert the trigger change

--- src/main/java/com/redhat/developers/GreetingResource.java
+++ src/main/java/com/redhat/developers/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "😺 Tekton Rocks!";
+        return "Hello World!";
     }
 }
